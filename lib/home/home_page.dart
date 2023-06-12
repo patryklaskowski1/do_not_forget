4a02970f817f4486a2411d813c35c1d2444aac8c
Patryk Laskowski <patrick_laskowski@hotmail.com>
2023-06-12 17:25:30 +0200

    Transfer code home page with main.dart to home_page.dart

---
+++ lib/home/home_page.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 114, 113, 113),
+      appBar: AppBar(
+        title: const Text(
+          'Do Not Forget',
+          style: TextStyle(
+            color: Color.fromARGB(255, 3, 253, 241),
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+            bottom: Radius.elliptical(MediaQuery.of(context).size.width, 80.0),
+          ),
+        ),
+        toolbarHeight: 120,
+        centerTitle: true,
+        backgroundColor: const Color.fromARGB(129, 41, 37, 37),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: const Color.fromARGB(129, 41, 37, 37),
+        child: const Icon(
+          Icons.add,
+          color: Color.fromARGB(255, 3, 253, 241),
+          size: 42,
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:do_not_forget/home/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,47 +13,6 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       title: 'Do Not Forget',
       home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 114, 113, 113),
-      appBar: AppBar(
-        title: const Text(
-          'Do Not Forget',
-          style: TextStyle(
-            color: Color.fromARGB(255, 3, 253, 241),
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.vertical(
-            bottom:
-                Radius.elliptical(MediaQuery.of(context).size.width, 80.0),
-          ),
-        ),
-        toolbarHeight: 120,
-        centerTitle: true,
-        backgroundColor: const Color.fromARGB(129, 41, 37, 37),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        backgroundColor: const Color.fromARGB(129, 41, 37, 37),
-        child: const Icon(
-          Icons.add,
-          color: Color.fromARGB(255, 3, 253, 241),
-          size: 42,
-        ),
-      ),
     );
   }
 }
