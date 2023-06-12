d65b9b3a393d26273b526495b7dba2035666be14
Patryk Laskowski <patrick_laskowski@hotmail.com>
2023-06-12 17:08:33 +0200

    Create AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -11,11 +11,26 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Do Not Forget',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Do Not Forget',
+            style: TextStyle(
+              color: Color.fromARGB(255, 3, 253, 241),
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(
+                bottom:
+                    Radius.elliptical(MediaQuery.of(context).size.width, 80.0)),
+          ),
+          toolbarHeight: 120,
+          centerTitle: true,
+          backgroundColor: const Color.fromARGB(129, 41, 37, 37),
+        ),
       ),
-      home: const Scaffold(),
     );
   }
 }
