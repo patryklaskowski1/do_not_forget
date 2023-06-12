51cb788883d5f81b6bfcfacb80e257805d6dd822
Patryk Laskowski <patrick_laskowski@hotmail.com>
2023-06-12 17:23:21 +0200

    Extract Scaffold to new Widget HomePage

--- lib/main.dart
+++ lib/main.dart
@@ -9,37 +9,48 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Do Not Forget',
-      home: Scaffold(
-        backgroundColor: const Color.fromARGB(255, 114, 113, 113),
-        appBar: AppBar(
-          title: const Text(
-            'Do Not Forget',
-            style: TextStyle(
-              color: Color.fromARGB(255, 3, 253, 241),
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.vertical(
-              bottom:
-                  Radius.elliptical(MediaQuery.of(context).size.width, 80.0),
-            ),
-          ),
-          toolbarHeight: 120,
-          centerTitle: true,
-          backgroundColor: const Color.fromARGB(129, 41, 37, 37),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          backgroundColor: const Color.fromARGB(129, 41, 37, 37),
-          child: const Icon(
-            Icons.add,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 114, 113, 113),
+      appBar: AppBar(
+        title: const Text(
+          'Do Not Forget',
+          style: TextStyle(
             color: Color.fromARGB(255, 3, 253, 241),
-            size: 42,
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
           ),
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+            bottom:
+                Radius.elliptical(MediaQuery.of(context).size.width, 80.0),
+          ),
+        ),
+        toolbarHeight: 120,
+        centerTitle: true,
+        backgroundColor: const Color.fromARGB(129, 41, 37, 37),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: const Color.fromARGB(129, 41, 37, 37),
+        child: const Icon(
+          Icons.add,
+          color: Color.fromARGB(255, 3, 253, 241),
+          size: 42,
         ),
       ),
     );
