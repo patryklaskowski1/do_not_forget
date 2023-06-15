8738cd03c0762ff44adfe8a3658b430240d302c9
Patryk Laskowski <patrick_laskowski@hotmail.com>
2023-06-15 16:26:29 +0200

    Create State for homepage

--- lib/home_page/cubit/home_page_cubit.dart
+++ lib/home_page/cubit/home_page_cubit.dart
@@ -1,4 +1,5 @@
 import 'package:bloc/bloc.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:meta/meta.dart';
 
 part 'home_page_state.dart';
--- lib/home_page/cubit/home_page_state.dart
+++ lib/home_page/cubit/home_page_state.dart
@@ -1,6 +1,14 @@
 part of 'home_page_cubit.dart';
 
 @immutable
- class HomePageState {}
-
+ class HomePageState {
+  const HomePageState({
+    this.items,
+    this.loadingErrorOccured = false,
+    this.removingErrorOccured = false,
+  });
+  final QuerySnapshot<Map<String, dynamic>>? items;
+  final bool loadingErrorOccured;
+  final bool removingErrorOccured;
+}
 
