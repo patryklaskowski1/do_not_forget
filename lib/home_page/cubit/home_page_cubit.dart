b9490601c419048c04630b05966d718fbe2141ac
Patryk Laskowski <patrick_laskowski@hotmail.com>
2023-06-15 18:17:14 +0200

    remove immutable in home page state

--- lib/home_page/cubit/home_page_cubit.dart
+++ lib/home_page/cubit/home_page_cubit.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:meta/meta.dart';
+
 
 part 'home_page_state.dart';
 
--- lib/home_page/cubit/home_page_state.dart
+++ lib/home_page/cubit/home_page_state.dart
@@ -1,6 +1,6 @@
 part of 'home_page_cubit.dart';
 
-@immutable
+
  class HomePageState {
   const HomePageState({
     this.items,
