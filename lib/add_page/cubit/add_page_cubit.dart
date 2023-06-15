66d39b70b706fd6264860dfd4412f4f3dc9ba18f
Patryk Laskowski <patrick_laskowski@hotmail.com>
2023-06-15 16:52:10 +0200

    add new event add in add page cubit

--- lib/add_page/cubit/add_page_cubit.dart
+++ lib/add_page/cubit/add_page_cubit.dart
@@ -1,8 +1,28 @@
 import 'package:bloc/bloc.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:meta/meta.dart';
 
 part 'add_page_state.dart';
 
 class AddPageCubit extends Cubit<AddPageState> {
   AddPageCubit() : super(AddPageState());
+
+  Future<void> add(
+    String title,
+    String description,
+    DateTime releaseDate,
+  ) async {
+    try {
+      await FirebaseFirestore.instance.collection('items').add(
+        {
+          'title': title,
+          'description': description,
+          'release_date': releaseDate,
+        },
+      );
+      emit(const AddPageState(saved: true));
+    } catch (error) {
+      emit(AddPageState(errorMessage: error.toString()));
+    }
+  }
 }
