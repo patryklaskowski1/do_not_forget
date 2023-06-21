97ae8d6383d3fe85f40a27a25a24f82b3ddd1849
Patryk Laskowski <patrick_laskowski@hotmail.com>
2023-06-21 17:36:54 +0200

    Add function get in items repository

--- lib/repositories/items_repository.dart
+++ lib/repositories/items_repository.dart
@@ -30,12 +30,23 @@ class ItemsRepository {
     String description,
     DateTime releaseDate,
   ) async {
-      await FirebaseFirestore.instance.collection('items').add(
-        {
-          'title': title,
-          'description': description,
-          'release_date': releaseDate,
-        },
-      );
+    await FirebaseFirestore.instance.collection('items').add(
+      {
+        'title': title,
+        'description': description,
+        'release_date': releaseDate,
+      },
+    );
+  }
+
+  Future<ItemModel> get({required String id}) async {
+    final doc =
+        await FirebaseFirestore.instance.collection('items').doc(id).get();
+    return ItemModel(
+      id: doc.id,
+      title: doc['title'],
+      description: doc['description'],
+      releaseDate: (doc['release_date'] as Timestamp).toDate(),
+    );
   }
 }
