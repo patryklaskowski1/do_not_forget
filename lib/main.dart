abae905d49a2d61980882d905a746ec5b2aa20b8
Patryk Laskowski <patrick_laskowski@hotmail.com>
2023-07-01 18:06:51 +0200

    Add Native Splash Screen

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,16 @@ import 'package:do_not_forget/firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
   runApp(const MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
